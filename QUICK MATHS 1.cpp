fa1dbb47ff54e98456c5e11a95f9fa2d20f1043d
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-02-22 09:27:56 +0700

    Add files via upload

---
+++ QUICK MATHS 1.cpp
@@ -0,0 +1,28 @@
+//Rini puspita wati
+//2257051019
+//A
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std ;
+int main(){
+    float r;
+    float i;
+    float n;
+    float p;
+    float w;
+    
+    cin>>r;
+    i = r+r;
+    cout << "" << r << " plus"<< " " << r << " is " << i << endl;
+    n = i-1;
+    cout << "Minus one is " << n <<endl;
+    p = n*3;
+    cout << "Multiple three is "<< p << endl;
+    w = p/2;
+    cout << "divide two is "<< w << endl;
+    return 0;
+           
+}
